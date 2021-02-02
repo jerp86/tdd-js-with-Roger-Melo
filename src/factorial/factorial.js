36f8cbe4b8c5259ff81116780d861fb5741235b3
Jose Eduardo Rodrigues Pinto (jerp86) <jerp4@hotmail.com>
2021-02-02 20:44:52 -0300

    ensure input 4 in getFactorial returned 24

--- src/factorial/factorial.js
+++ src/factorial/factorial.js
@@ -1,1 +1,8 @@
-export default () => 6;
+export default number => {
+  let factorial = 1;
+  for (let i = number; i >= 1; i--) {
+    factorial = factorial * i;
+  }
+
+  return factorial;
+};
--- src/factorial/factorial.test.js
+++ src/factorial/factorial.test.js
@@ -10,3 +10,10 @@ test('getFactorial(3) should return 6', () => {
 
   expect(actual).toBe(expected);
 });
+
+test('getFactorial(4) should return 24', () => {
+  const actual = getFactorial(4);
+  const expected = 24;
+
+  expect(actual).toBe(expected);
+});
