43127c1dab5ef3241f6996185dbcafc4456d3d02
Jose Eduardo Rodrigues Pinto (jerp86) <jerp4@hotmail.com>
2021-02-02 20:44:08 -0300

    ensure input 3 in getFactorial returned 6

--- src/factorial/factorial.test.js
+++ src/factorial/factorial.test.js
@@ -3,3 +3,10 @@ import getFactorial from './factorial';
 test('getFactorial should be a function', () => {
   expect(getFactorial).toBeInstanceOf(Function);
 });
+
+test('getFactorial(3) should return 6', () => {
+  const actual = getFactorial(3);
+  const expected = 6;
+
+  expect(actual).toBe(expected);
+});
